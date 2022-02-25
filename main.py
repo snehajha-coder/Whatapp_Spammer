5b7576257514a243daef68286515f72fd34d9ef7
snehajha-coder <84180023+snehajha-coder@users.noreply.github.com>
2022-02-25 13:33:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,28 @@
-import  time
-#from datetime import date, datetime
+import  time    
+from selenium import webdriver   #used selenium to automate my chrome browser
+from selenium.webdriver.common.keys import Keys        #imported keyboards
 
-#import pyautogui
-from selenium import webdriver
-#import pyautogui
-#from selenium.common.exceptions import NoSuchElementException
-#from keyboard import press
-from selenium.webdriver.common.keys import Keys
 
-options = webdriver.ChromeOptions()
-options.add_argument(r'--user-data-dir=C:\Users\Hp\AppData\Local\Google\Chrome\User Data\Default')
+options = webdriver.ChromeOptions()            
+options.add_argument(r'--user-data-dir=C:\Users\Hp\AppData\Local\Google\Chrome\User Data\Default')     #path where user recent data is stored so that you don't need to spam again and again
 options.add_argument('--profile-directory=Default')
 
-chrome_browser = webdriver.Chrome(executable_path=r'D:/softwares/edge web driver/chromedriver.exe', options=options)
-chrome_browser.get('https://web.whatsapp.com/')
+chrome_browser = webdriver.Chrome(executable_path=r'D:/softwares/edge web driver/chromedriver.exe', options=options)  #will open webdriver
+chrome_browser.get('https://web.whatsapp.com/')  #will go to WhatApp web
 
-time.sleep(20)
+time.sleep(20)  # will wait for 20s helpfull if internet speed is slow and to reduce errors which will cause due to hurry
 
-user_name_list = ['AMBUJ -1 Chem', 'Kshitij Chem -1']
-msg = '🙂'
+user_name_list = ['AMBUJ -1 Chem', 'Kshitij Chem -1'] #list of peoples you want to disturb name exactly same as you saved in WhatApp
+msg = '🙂' #the msg you want to send repetatively
 
 
-for user_name in user_name_list:
+for user_name in user_name_list:  #will go to each friend one by one
 
-    user = chrome_browser.find_element_by_xpath('//span[@title ="{}"]'.format(user_name))
+    user = chrome_browser.find_element_by_xpath('//span[@title ="{}"]'.format(user_name))   #will find the person
     user.click()
-    #enter_key = chrome_browser.find_element_by_xpath('//button[@class="_4sWnG"]')
-
     time.sleep(20)
-    message_box = chrome_browser.find_element_by_xpath('//div[@class="p3_M1"]')
+    message_box = chrome_browser.find_element_by_xpath('//div[@class="p3_M1"]')        # will find the msg box
 
-    for i in range(30000):
-        message_box.send_keys(msg + Keys.ENTER)
+    for i in range(3000):   #will send that msg 3000 times to each of them
+        message_box.send_keys(msg + Keys.ENTER)           #will type and send msg
         print(i)
-
-        #pyautogui.press('enter')
-        #enter_key = chrome_browser.find_element_by_xpath('//button[@class="_4sWnG"]')
-        #enter_key.click()
-
